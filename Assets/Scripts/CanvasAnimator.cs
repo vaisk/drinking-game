98e5eceb0abd0d97fa8bd77272d0201e38a14e2b
zischler <tobias.zischler@gmx.ch>
2019-11-03 15:09:42 +0100

    changed styling again and added animations

---
+++ Assets/Scripts/CanvasAnimator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasAnimator : MonoBehaviour
+{
+	//Animator animator;
+
+ //   // Start is called before the first frame update
+ //   void Start()
+ //   {
+	//	animator = GetComponent<Animator>();
+	//}
+
+ //   // Update is called once per frame
+ //   void Update()
+ //   {
+        
+ //   }
+
+ //   void OnDestroy()
+	//{
+	//	animator.setTrigger("FadeOut");
+	//}
+}
