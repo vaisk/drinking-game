c95b9cc011802353e8c68fdfffac7b4a655da789
zischler <tobias.zischler@gmx.ch>
2019-11-03 11:21:15 +0100

    added dare scene

---
+++ Assets/Scripts/RandomMultiplier.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class RandomMultiplier : MonoBehaviour
+{
+    public TextMeshProUGUI Multiplier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        System.Random random = new System.Random();
+        int multiplier = random.Next(6);
+        Multiplier.text = "x" + (multiplier + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
